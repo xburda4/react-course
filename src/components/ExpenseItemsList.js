720ac49748bbb8d4238c88b40045a66a03df56ca
Marek Burda <m.burda583@gmail.com>
2021-05-30 18:44:46 +0200

    Assignment 1

---
+++ src/components/ExpenseItemsList.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./ExpenseItem";
+import './ExpenseItemsList.css'
+
+function ExpenseItemsList(props) {
+    return (
+        <div className="expenses">
+            {props.expenses.map(o => <ExpenseItem title={o.title} amount={o.amount} date={o.date}/>)}
+        </div>
+    )
+}
+
+export default ExpenseItemsList
